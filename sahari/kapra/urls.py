850c2ce4b5ae84891a77fb7dcef3646c2a41e1be
sandeeparyal <sandeeparyal@localhost.localdomain>
2016-03-13 16:34:16 +0545

    Some Changes

--- sahari/kapra/urls.py
+++ sahari/kapra/urls.py
@@ -7,7 +7,7 @@ from kapra import views
 urlpatterns = patterns('',
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'(?P<ministry_id>\d+)/$', views.headsection_listings, name='headsection_listings'),
-    url(r'sections/(?P<hsection>\d+)/$', views.section_listings, name='section_listings'),
-    url(r'(?P<hsection>[\w\s]+)/(?P<section>[\w\s]+)/$', views.employee_listings, name='employee_listings'),
+    url(r'(?P<hsection_id>\d+)/$', views.section_listings, name='section_listings'),
+    url(r'(?P<headsection_id>\d+)/(?P<section_id>\d+)/$', views.employee_listings, name='employee_listings'),
     )
 
--- sahari/kapra/views.py
+++ sahari/kapra/views.py
@@ -23,14 +23,13 @@ def headsection_listings(request, ministry_id): #here word has in it the id from
     context = {'head_section_list':head_section_list}
     return render(request, 'kapra/headsection_list.html', context)
 
-def section_listings(request, hsection):
-    section_list = Section.objects.all()
-    section_name = [p.section_name for p in section_list if p.headsection.head_section_name == hsection]
-    context = {'section_name':section_name, 'hsection':hsection}
+def section_listings(request, headsection_id):
+    section_list = Section.objects.filter(headsection__id=headsection_id)
+    context = {'section_list':section_list, 'headsection_id':headsection_id }
     return render(request, 'kapra/section_list.html', context)
 
-def employee_listings(request, hsection, section):
-    employee_list = Employee.objects.all()
+def employee_listings(request, headsection_id, section_id):
+    employee_list = Employee.objects.filter(section__id=section)
     employee_id = [p.emp_id for p in employee_list if p.section.section_name == section]
     employee_name = [p.emp_first_name +' ' +  p.emp_last_name for p in employee_list if p.section.section_name == section]
     context = {'employee_name':employee_name, 'section':section, 'employee_id':employee_id}
