efda6c10654f2552f0d7c4ef7cea7101758201d1
sandeeparyal <sandeeparyal@localhost.localdomain>
2016-03-15 11:14:20 +0545

    Haakim_Japan

--- sahari/kapra/views.py
+++ sahari/kapra/views.py
@@ -23,15 +23,15 @@ def headsection_listings(request, ministry_id): #here word has in it the id from
     context = {'head_section_list':head_section_list}
     return render(request, 'kapra/headsection_list.html', context)
 
-def section_listings(request, headsection_id):
-    section_list = Section.objects.filter(headsection__id=headsection_id)
-    context = {'section_list':section_list, 'headsection_id':headsection_id }
+def section_listings(request, head_section_id):
+    section_list = Section.objects.filter(headsection__id=head_section_id)
+    context = {'section_list':section_list, 'head_section_id':head_section_id }
     return render(request, 'kapra/section_list.html', context)
 
-def employee_listings(request, headsection_id, section_id):
-    employee_list = Employee.objects.filter(section__id=section)
-    employee_id = [p.emp_id for p in employee_list if p.section.section_name == section]
-    employee_name = [p.emp_first_name +' ' +  p.emp_last_name for p in employee_list if p.section.section_name == section]
+def employee_listings(request, head_section_id, section_id):
+    employee_list = Employee.objects.filter(section__id=section_id)
+    employee_id = [p.emp_id for p in employee_list]
+    employee_name = [p.emp_first_name +' ' +  p.emp_last_name for p in employee_list]
     context = {'employee_name':employee_name, 'section':section, 'employee_id':employee_id}
     return render(request, 'kapra/employee_list.html', context)
 
