15227f27e272fd368e9c6c54f18608f68135cedc
sandeeparyal <sandeeparyal@localhost.localdomain>
2016-03-15 12:07:27 +0545

    First Success

--- sahari/kapra/urls.py
+++ sahari/kapra/urls.py
@@ -6,8 +6,8 @@ from kapra import views
 
 urlpatterns = patterns('',
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'(?P<ministry_id>\d+)/$', views.headsection_listings, name='headsection_listings'),
-    url(r'(?P<head_section_id>\d+)/$', views.section_listings, name='section_listings'),
-    url(r'(?P<head_section_id>\d+)/(?P<section_id>\d+)/$', views.employee_listings, name='employee_listings'),
+    url(r'hs(?P<ministry_id>\d+)/$', views.headsection_listings, name='headsection_listings'),
+    url(r's(?P<ministry_id>\d+)/(?P<head_section_id>\d+)/$', views.section_listings, name='section_listings'),
+    url(r'e(?P<ministry_id>\d+)/(?P<head_section_id>\d+)/(?P<section_id>\d+)/$', views.employee_listings, name='employee_listings'),
     )
 
--- sahari/kapra/views.py
+++ sahari/kapra/views.py
@@ -20,19 +20,19 @@ class IndexView(generic.ListView):
 
 def headsection_listings(request, ministry_id): #here word has in it the id from the ministry name
     head_section_list = HeadSection.objects.filter(ministry__id=int(ministry_id))
-    context = {'head_section_list':head_section_list}
+    context = {'head_section_list':head_section_list, 'ministry_id':ministry_id}
     return render(request, 'kapra/headsection_list.html', context)
 
-def section_listings(request, head_section_id):
-    section_list = Section.objects.filter(headsection__id=head_section_id)
-    context = {'section_list':section_list, 'head_section_id':head_section_id }
+def section_listings(request, ministry_id, head_section_id):
+    section_list = Section.objects.filter(headsection__id=int(head_section_id))
+    context = {'section_list':section_list, 'head_section_id':head_section_id, 'ministry_id':ministry_id }
     return render(request, 'kapra/section_list.html', context)
 
-def employee_listings(request, head_section_id, section_id):
-    employee_list = Employee.objects.filter(section__id=section_id)
+def employee_listings(request, ministry_id, head_section_id, section_id):
+    employee_list = Employee.objects.filter(section__id=int(section_id))
     employee_id = [p.emp_id for p in employee_list]
     employee_name = [p.emp_first_name +' ' +  p.emp_last_name for p in employee_list]
-    context = {'employee_name':employee_name, 'section':section, 'employee_id':employee_id}
+    context = {'employee_name':employee_name, 'employee_id':employee_id}
     return render(request, 'kapra/employee_list.html', context)
 
 
